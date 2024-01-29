c9110cedd819a3c5e579d8e83dd3e2190d8a8d65
= <dhoriffitodiansyah27@gmail.com>
2024-01-29 13:05:32 +0700

    delete some file outside project

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,13 @@
 use App\Http\Controllers\AnggotaController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\IndikatorKinerjaController;
 use App\Http\Controllers\IndikatorProgramController;
 use App\Http\Controllers\JaketController;
 use App\Http\Controllers\MasterSubController;
 use App\Http\Controllers\OthersController;
 use App\Http\Controllers\PendakiController;
+use App\Http\Controllers\PengaturanController;
 use App\Http\Controllers\SepatuController;
 use App\Http\Controllers\SleepingBagController;
 use App\Http\Controllers\TendaController;
@@ -29,19 +31,13 @@ Auth::routes();
 
 Route::middleware(['auth'])->group(function(){
     Route::get('/', [HomeController::class, 'index']);
-    Route::get('/anggotakelompok', [AnggotaController::class, 'index']);
-    Route::resource('/datatenda', TendaController::class);
-    Route::resource('/pendaki', PendakiController::class);
-    Route::resource('/sb', SleepingBagController::class);
-    Route::resource('/jaket', JaketController::class);
-    Route::resource('/sepatu', SepatuController::class);
-    Route::resource('/sb', SleepingBagController::class);
-    Route::resource('/others', OthersController::class);
 
     Route::resource('/master_subkegiatan', MasterSubController::class);
-
+    Route::resource('/indikator_program', IndikatorProgramController::class);
+    Route::resource('/indikator_kinerja', IndikatorKinerjaController::class);
+    Route::resource('/pengaturan', PengaturanController::class);
 });
-Route::resource('/indikator_program', IndikatorProgramController::class);
+
 
 Route::get('logout', [LoginController::class, 'logout']);
 
