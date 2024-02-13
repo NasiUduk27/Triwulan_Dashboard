f6a856425c62da9e7a62c90c41e34e303da52e82
NasyawaRamadhiaKiranaSavaEffendi <ramadhianasyawa@gmail.com>
2024-02-13 17:14:23 +0700

    routes update

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\BerandaController;
 use App\Http\Controllers\IndikatorKegiatanController;
 use App\Http\Controllers\IndikatorProgramController;
-use App\Http\Controllers\KegiatanController;
 use App\Http\Controllers\IndikatorKinerjaController;
+use App\Http\Controllers\KegiatanController;
+use App\Http\Controllers\MasterKegiatanController;
 use App\Http\Controllers\MasterProgramController;
 use App\Http\Controllers\MasterSubController;
 use App\Http\Controllers\PengaturanController;
@@ -29,6 +30,7 @@ Auth::routes();
 Route::middleware(['auth'])->group(function () {
     Route::get('/', [BerandaController::class, 'index']);
     Route::resource('/master_program', MasterProgramController::class);
+    Route::resource('/beranda', BerandaController::class);
     Route::resource('/master_kegiatan', KegiatanController::class);
     Route::resource('/master_subkegiatan', MasterSubController::class);
     Route::resource('/indikator_program', IndikatorProgramController::class);
@@ -36,6 +38,6 @@ Route::middleware(['auth'])->group(function () {
     Route::resource('/indikator_kinerja', IndikatorKinerjaController::class);
     Route::resource('/pengaturan', PengaturanController::class);
     Route::resource('/realisasi', RealisasiController::class);
-
+    Route::get('/get-kegiatan', [KegiatanController::class, 'getKegiatan']);
 });
 Route::get('logout', [LoginController::class, 'logout']);
